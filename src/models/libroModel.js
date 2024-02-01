66dbd1fdf89e8204d29c4a4ff174f83511c171b8
Sonia <soniaferreyrapesa@gmail.com>
2024-02-01 20:21:07 -0300

    .

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -8,6 +8,7 @@ mongoose.connect(process.env.MONGO_DB, {
 const LibroSchema = new mongoose.Schema({
   titulo: String,
   autor: String
+
 }, { collection: 'libros' });
 
 const Libro = mongoose.model('Libro', LibroSchema);
