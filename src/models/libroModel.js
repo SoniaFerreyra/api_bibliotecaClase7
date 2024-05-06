5f4c4a82992b36b0cf1fdf6933bcee5a26982232
Sonia <soniaferreyrapesa@gmail.com>
2024-05-06 12:43:28 -0300

    actualizacon

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,22 +1,16 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-<<<<<<< HEAD
-mongoose.connect('mongodb://localhost:27017/biblioteca', {
-=======
-console.log(process.env.MONGO_DB);
+console.log(process.env.MONGO_DB); 
+
 mongoose.connect(process.env.MONGO_DB, {
->>>>>>> c3045eb11f4edd2302bcae71d89c06c321acfe14
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-  
-
 
 const LibroSchema = new mongoose.Schema({
   titulo: String,
   autor: String
-
 }, { collection: 'libros' });
 
 const Libro = mongoose.model('Libro', LibroSchema);
